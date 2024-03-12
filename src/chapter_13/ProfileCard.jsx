e80d175ef180f0f6c61fa205a97bd760c29b3ab8
Lee Sunjae <sunjae7944@naver.com>
2024-03-12 22:11:59 +0900

    0312

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="React" backgroundColor="#4ea04e">
+            <p>안녕하세요</p>
+            <p>저는 리액트를 잘하고 싶습니다.</p>
+        </Card>
+    );
+}
+export default ProfileCard;
